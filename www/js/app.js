b9e4f26879e948ed196d758e414bf664ea41d5d8
Adam Menz <mz.adam@gmail.com>
2014-06-25 11:38:02 -0700

    Got the Nav/Tab bar working with back

---
+++ www/js/app.js
@@ -0,0 +1,87 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('jobs_page', {
+    url: '/Jobs',
+    templateUrl: 'jobs_page.html',
+    controller : "JobsCtrl"
+  })
+  .state('notes_page', {
+    url: '/Notes',
+    templateUrl: 'notes_page.html',
+    controller : "NotesCtrl"
+  })
+  .state('alerts_page', {
+    url: '/Alerts',
+    templateUrl: 'alerts_page.html',
+    controller : "AlertsCtrl"
+  })
+  
+  $urlRouterProvider.otherwise("/Jobs");
+})
+
+.controller('JobsCtrl', function($scope) {
+  
+})
+.controller('NotesCtrl', function($scope) {
+  
+})
+.controller('AlertsCtrl', function($scope) {
+  
+})
+
+.directive('back', function () {
+    return {
+        restrict: 'E',
+        link: function(scope, element) {
+          element.on('click', function() {
+            window.history.back();
+          });
+        },
+        controller: 'NavCtrl'
+    };
+});
+
+function NavCtrl($scope) {
+  
+  $scope.goBack = function() {
+    console.log("HIH");
+    $ionicNavBarDelegate.back();
+  };
+
+  $scope.getClass = function(path) {
+
+    if (window.location.href.indexOf(path) != -1) {
+      return "active"
+    } else {
+      return ""
+    }
+    // if (location.path().substr(0, path.length) == path) {
+    //   return "active"
+    // } else {
+    //   return ""
+    // }
+}
+
+}
+
+
