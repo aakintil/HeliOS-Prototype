74511a9c7f9207bb2a3edb235f736f18383b500f
aakintil <aakintil@andrew.cmu.edu>
2014-07-02 13:55:56 -0700

    added get, post, and delete routes for the Notes model

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
 
 // server.js
 
-// set up ========================
+// =============== Set Up ==================
 var express  = require('express');
 var app      = express(); 								// create our app w/ express
 var mongoose = require('mongoose'); 					// mongoose for mongodb
@@ -22,6 +22,72 @@ app.configure(function() {
 	app.use(express.logger('dev')); 						// log every request to the console
 	app.use(express.bodyParser()); 							// pull information from html in POST
 });
+
+// =============== RESTful API ================
+
+// Notes Model 
+var Note = mongoose.model( 'Note', {
+	text : String 
+})
+
+
+// == Routes ==
+
+
+// Get ALL Notes
+app.get( '/api/notes', function ( req, res ) {
+
+	// use mongoose to get all notes in the database
+	Note.find( function( err, notes ) {
+
+		// if there is an error, send the error notification 
+		if ( err ) { res.send( err ); console.log("Error finding / getting appropriate note  |  line 42 : server.js") }; 
+
+		res.json( notes ) // return all notes in the JSON format
+	})
+}); 
+
+// Create a Note
+app.post( '/api/notes', function( req, res ) {
+
+	// create a todo, information comes from AJAX request from Angular 
+	// ===== SET THIS UP ===== 
+
+	Note.create({ 
+		text : req.body.text; 
+		done : false; 
+	}, function( err, note ) {
+		if ( err ) { res.send( err ); console.log("Error creating / inserting appropriate note  |  line 57 : server.js") }; 
+
+		Note.find( function( err, notes ) {
+			if ( err ) { res.send( err ); console.log("Error finding / getting appropriate note AFTER CREATING one |  line 60 : server.js") };
+			res.json( notes ) // return all notes in the JSON format after we create another
+		})
+	})
+}); 
+
+
+// Delete a Note 
+// think about making this "HIDE" ... which if we do, an api call won't be necessary 
+app.delete( '/api/notes/:note_id', function( req, res ) {
+	Note.remove({
+		_id : req.params.note_id
+	}, function( err, note ) {
+		if ( err ) { res.send( err ); console.log("Error deleting appropriate note |  line 77 : server.js") }
+		
+		Note.find( function( err, notes ) {
+			if ( err ) { res.send( err ); console.log("Error finding / getting appropriate note |  line 80 : server.js") }
+			
+			res.json( notes ) // return all notes in the JSON format after we create another
+		})
+
+	})
+})
+
+
+
+
+
 
 // listen (start app with node server.js) ======================================
 app.listen(8080);
